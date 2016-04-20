a5eb3c0a0839b2cd11792e1ed6f4a66354895c8b
Victor Gama <hey@vito.io>
2016-04-20 13:53:10 -0300

    Update project definition

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cloom "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject cloom "0.1.1-SNAPSHOT"
+  :description "boom, but in Clojure"
+  :url "https://github.com/victorgama/cloom"
+  :license {:name "MIT"
+            :url "https://github.com/victorgama/cloom/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "0.3.3"]]
